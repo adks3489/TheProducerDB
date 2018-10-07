8daaba4589eb4c37ca807c16f79b08a9f570e272
James Chiang <adks3489@gmail.com>
2018-10-07 21:49:28 +0800

    增加演員性別、區域

--- src/CalcPage.jsx
+++ src/CalcPage.jsx
@@ -18,6 +18,11 @@ class CalcPage extends Component {
           continue;
         let idx = 0;
         for( let chara of article.Characters){
+          if((chara[4] != null && actor.Gender != null && chara[4] != actor.Gender) ||
+             (chara[5] != null && actor.Nation != null && chara[5] != actor.Nation) ){
+            ++idx;
+            continue;
+          }
           let result = {ScriptType: false, ScriptTag: [], CharaTag: []};
           result.ScriptType = actor.FilmTypes.includes(article.Type);
           result.ScriptTag = article.Tags.filter(tag=>actor.FilmTags.includes(tag));
@@ -53,13 +58,16 @@ class CalcPage extends Component {
                   {o[6]!=null ? CHARA_FACTION[o[6]]: ""}
                   {o[7]!=null ? CHARA_TYPE[o[7]]: ""}
                   {charaResult[i].map((result, i)=>(
-                    <div key={i} style={{marginTop:"10px"}}>
-                      <b>{result.Actor.Name}</b>
-                      <Badge style={{ backgroundColor: '#52c41a' }} count={result.Result.ScriptTag.length+result.Result.ScriptType?1:0}/>
-                      <Badge style={{ backgroundColor: '#52c41a' }} count={result.Result.CharaTag.length}/><br/>
-                      片型: {result.Result.ScriptType ? "符合" : "不符合"}<br/>
-                      劇本符合: {result.Result.ScriptTag.map(o=><Tag color="#108ee9" key={o}>{FILM_TAG[o]}</Tag>)}<br/>
-                      角色符合: {result.Result.CharaTag.map(o=><Tag color="purple" key={o}>{CHARA_TAG[o]}</Tag>)}
+                    <div key={i} style={{marginTop:"10px", display:"flex", flexDirection:"row", alignItems:"center"}}>
+                      <img src={`images/actor${result.Actor.key}.jpg`} style={{float:"left", height:"75px"}} />
+                      <div style={{flex:"1", float:"left", marginLeft: "5px"}}>
+                        <b>{result.Actor.Name}</b>
+                        <Badge style={{ backgroundColor: '#52c41a' }} count={result.Result.ScriptTag.length+(result.Result.ScriptType?1:0)}/>
+                        <Badge style={{ backgroundColor: '#52c41a' }} count={result.Result.CharaTag.length}/><br/>
+                        片型: {result.Result.ScriptType ? "符合" : "不符合"}<br/>
+                        劇本: {result.Result.ScriptTag.map(o=><Tag color="#108ee9" key={o}>{FILM_TAG[o]}</Tag>)}<br/>
+                        角色: {result.Result.CharaTag.map(o=><Tag color="purple" key={o}>{CHARA_TAG[o]}</Tag>)}
+                      </div>
                     </div>
                   ))}
                 </Col>))}
